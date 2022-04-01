bfa2e4c6ec73f22762a58985264edb56684b30ac
Renish <renish2399@gmail.com>
2022-04-01 21:22:28 +0530

    added proxy of cloud backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
   const [stripeApiKey, setStripeApiKey] = useState("");
 
   async function getStripeApiKey() {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/v1/stripeapikey`);
+    const { data } = await axios.get(`/api/v1/stripeapikey`);
 
     setStripeApiKey(data.stripeApiKey);
   }
--- frontend/src/component/Cart/Payment.js
+++ frontend/src/component/Cart/Payment.js
@@ -57,7 +57,7 @@ const Payment = ({ history }) => {
         },
       };
       const { data } = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/api/v1/payment/process`,
+        `/api/v1/payment/process`,
         paymentData,
         config
       );
